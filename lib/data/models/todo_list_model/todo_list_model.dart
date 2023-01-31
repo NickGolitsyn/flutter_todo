e1f06a6a6e3782de886e87b415e3ac3088332151
Nick Golitsyn <nickgolitsyn@nicks-air.home>
2023-01-31 19:15:17 +0000

    first commit

---
+++ lib/data/models/todo_list_model/todo_list_model.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../todo_model/todo_model.dart';
+
+part 'todo_list_model.g.dart';
+
+@JsonSerializable()
+class TodoListModel {
+  TodoListModel(this.todoListModel);
+  List<TodoModel>? todoListModel;
+
+  factory TodoListModel.fromJson(Map<String, dynamic> json) => _$TodoListModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TodoListModelToJson(this);
+}
